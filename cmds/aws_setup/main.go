1036ca859a2c19fb0c767035e330e26c7bf90038
www <wayming.z@gmail.com>
2018-06-14 23:23:26 +1000

    List subnets

--- cmds/aws_setup/main.go
+++ cmds/aws_setup/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"os"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
@@ -13,41 +13,22 @@ import (
 const cmdCreateVPC = "CreateVPC"
 const cmdDeleteVPC = "DeleteVPC"
 const cmdListVPC = "ListVPC"
+const cmdCreateDefaultVPC = "CreateDefaultVPC"
 
-func createVPC() {
-	var session, err = session.NewSession(&aws.Config{Region: aws.String("ap-southeast-2")})
-	input := &ec2.CreateVpcInput{
-		CidrBlock: aws.String("10.0.0.0/16"),
-	}
-	fmt.Println("VPC created")
-
-	svc := ec2.New(session)
-	result, err := svc.CreateVpc(input)
-	if err != nil {
-		if aerr, ok := err.(awserr.Error); ok {
+func printAWSError(e error) {
+	if e != nil {
+		if aerr, ok := e.(awserr.Error); ok {
 			switch aerr.Code() {
 			default:
 				fmt.Println(aerr.Error())
 			}
 		} else {
-			// Print the error, cast err to awserr.Error to get the Code and
-			// Message from an error.
-			fmt.Println(err.Error())
+			fmt.Println(e.Error())
 		}
-		return
 	}
-
-	fmt.Println(result)
 }
 
-func deleteVPC() {
-	svc := ec2.New(session.New())
-	fmt.Println(svc)
-
-}
-
-func listVPC() {
-	svc := ec2.New(session.New())
+func getVPC(awsSession *session.Session) *ec2.DescribeVpcsOutput {
 	input := &ec2.DescribeVpcsInput{
 		Filters: []*ec2.Filter{
 			&ec2.Filter{
@@ -57,34 +38,117 @@ func listVPC() {
 		},
 	}
 
-	result, err := svc.DescribeVpcs(input)
+	result, err := ec2.New(awsSession).DescribeVpcs(input)
 	if err != nil {
-		if aerr, ok := err.(awserr.Error); ok {
-			switch aerr.Code() {
-			default:
-				fmt.Println(aerr.Error())
-			}
-		} else {
-			// Print the error, cast err to awserr.Error to get the Code and
-			// Message from an error.
-			fmt.Println(err.Error())
-		}
+		printAWSError(err)
+		return nil
+	}
+
+	return result
+}
+
+func getSubnets(awsSession *session.Session, vpc string) *ec2.DescribeSubnetsOutput {
+	input := &ec2.DescribeSubnetsInput{
+		Filters: []*ec2.Filter{
+			&ec2.Filter{
+				Name:   aws.String("vpc-id"),
+				Values: []*string{aws.String(vpc)},
+			},
+		},
+	}
+
+	result, err := ec2.New(awsSession).DescribeSubnets(input)
+	if err != nil {
+		printAWSError(err)
+		return nil
+	}
+
+	return result
+}
+
+func createDefaultVPC(awsSession *session.Session) {
+	input := &ec2.CreateDefaultVpcInput{}
+	result, err := ec2.New(awsSession).CreateDefaultVpc(input)
+	if err != nil {
+		printAWSError(err)
+		return
+	}
+
+	fmt.Println("VPC created")
+	fmt.Println(result)
+}
+
+func createVPC(awsSession *session.Session) {
+	input := &ec2.CreateVpcInput{
+		CidrBlock: aws.String("10.0.0.0/16"),
+	}
+	result, err := ec2.New(awsSession).CreateVpc(input)
+	if err != nil {
+		printAWSError(err)
+		return
+	}
+
+	fmt.Println("VPC created")
+	fmt.Println(result)
+}
+
+func deleteVPC(awsSession *session.Session, name string) {
+	input := &ec2.DeleteVpcInput{
+		VpcId: aws.String(name),
+	}
+	result, err := ec2.New(awsSession).DeleteVpc(input)
+	if err != nil {
+		printAWSError(err)
 		return
 	}
 
 	fmt.Println(result)
-}
-func main() {
 
-	var command = os.Args[1]
+}
+
+func listVPC(awsSession *session.Session) {
+	vpcs := getVPC(awsSession)
+	fmt.Println(vpcs)
+
+	for _, vpc := range vpcs.Vpcs {
+		listSubnets(awsSession, *vpc.VpcId)
+	}
+
+}
+
+func listSubnets(awsSession *session.Session, vpc string) {
+	subnets := getSubnets(awsSession, vpc)
+	fmt.Println(subnets)
+}
+
+func main() {
+	command := flag.String(
+		"exec", cmdListVPC,
+		"ListVPC, CreateDefaultVPC, CreateVPC or DeleteVPC")
+	vpcName := flag.String("vpc", "", "name of the VPC")
+	flag.Parse()
+
+	if *command == cmdDeleteVPC && *vpcName == "" {
+		fmt.Println("Please specify the name of vpc")
+		return
+	}
+
+	var session, err = session.NewSession(&aws.Config{Region: aws.String("ap-southeast-2")})
+	if err != nil {
+		printAWSError(err)
+		return
+	}
+
 	fmt.Println(command)
-	switch command {
+	switch *command {
 	case cmdCreateVPC:
-		fmt.Print("Not supported yet")
+		createVPC(session)
+	case cmdCreateDefaultVPC:
+		createDefaultVPC(session)
 	case cmdDeleteVPC:
-		fmt.Print("Not supported yet")
+		deleteVPC(session, *vpcName)
 	case cmdListVPC:
-		listVPC()
+		listVPC(session)
 	default:
 		fmt.Println("Unknown command")
 	}
